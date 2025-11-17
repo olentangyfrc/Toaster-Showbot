6aab44dcf6d0a23746079861fd430f320d24b8ee
Akshaj Katkuri <akshaj2312@gmail.com>
2025-11-17 18:42:56 -0500

    added cancel command

--- robot.py
+++ robot.py
@@ -144,16 +144,11 @@ class MyRobot(MagicRobot):
         elif self.drivetrain.is_motion_limited():
             self.drivetrain.disable_motion_limiting()
 
-        if self.controller.getAButtonPressed():
-            self.drivetrain.go_to_pose(Pose2d(2, 3, Rotation2d.fromDegrees(180)))
-        elif self.controller.getBButtonPressed():
-            self.drivetrain.go_to_pose(Pose2d(1, 2, Rotation2d.fromDegrees(180)))
-
         if self.controller.getYButtonPressed():
             self.drivetrain.gyro.set_yaw(0)
 
-        if self.controller.getStartButton() and not self.intake.has_note():
-            self.intake.go_to_idle()
+        if self.controller.getStartButton():
+            self.cancel_all()
 
         if (
             self.controller.getRightTriggerAxis() > 0.2
@@ -173,9 +168,6 @@ class MyRobot(MagicRobot):
         ):
             self.shooter.shoot()
 
-
-        if self.controller.getLeftBumperPressed():
-            self.climber.set_manual_voltage(-2)
 
     def robotPeriodic(self) -> None:
         # Stops unimportant notifications during comp
@@ -231,3 +223,8 @@ class MyRobot(MagicRobot):
             ),
             lambda: RobotController.isBrownedOut(),
         )
+
+    def cancel_all(self) -> None: 
+        if (not self.intake.has_note()) and (not self.shooter.has_note()): 
+            self.intake.go_to_idle()
+            self.shooter.go_to_idle()
